01bfa049c68837c7f3fa2c6e24cb1343becd3863
Javi-HA <61690146+Javi-HA@users.noreply.github.com>
2024-01-28 12:01:40 +0100

    changes

--- GGJ24_Game/Assets/Scripts/Pickups/InteractivePickupBehaviour.cs
+++ GGJ24_Game/Assets/Scripts/Pickups/InteractivePickupBehaviour.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class InteractivePickupBehaviour : InteractiveBehaviourBase
 {
+    [FormerlySerializedAs("noiseCollider")] [SerializeField] private SphereCollider soundRadius;
     protected override void CompleteInteraction()
     {
         base.CompleteInteraction();
 
         gameObject.transform.GetChild(0).gameObject.SetActive(false);
         GetComponent<AudioSource>().enabled = true;
-
+        StartCoroutine(BroadcastNoise());
         Invoke("Destroy", 2.9f);
+        
+        
+    }
+
+    IEnumerator BroadcastNoise()
+    {
+        soundRadius.gameObject.SetActive(true);
+        yield return null;
+        yield return null;
+        soundRadius.gameObject.SetActive(false);
     }
 
     void Destroy()
