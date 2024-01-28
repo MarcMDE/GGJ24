3a388311333bb6424353580981ae7393e006ef05
Enya <102071587+Goorsic@users.noreply.github.com>
2024-01-28 15:46:40 +0100

    main menu

--- GGJ24_Game/Assets/Sprites/UI/MenuFun.cs
+++ GGJ24_Game/Assets/Sprites/UI/MenuFun.cs
@@ -1,10 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuFun : MonoBehaviour
 {
-   
 
+    public GameObject credits;
+    public GameObject menu;
+    public GameObject bgcredits;
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("MainScene");
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+
+    }
+
+    public void Credits()
+    {
+        bgcredits.SetActive(true);
+        credits.gameObject.SetActive(true);
+        menu.gameObject.SetActive(false);
+    }
+
+    public void CreditsExit()
+    {
+        bgcredits.SetActive(false);
+        credits.gameObject.SetActive(false);
+        menu.gameObject.SetActive(true);
+
+    }
 
 }
